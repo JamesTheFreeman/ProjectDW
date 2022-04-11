b32ceac6161da3182ea77bf6cbb0e891be3ec85d
Jamesrkiv <jamesrkiv@gmail.com>
2022-04-11 15:11:21 -0400

    Added more props/door

--- Assets/Scripts/door.cs
+++ Assets/Scripts/door.cs
@@ -95,12 +95,12 @@ public class door : MonoBehaviour
             */
             player.SetActive(false);                                                                // Disable player
             try{currRoom.GetComponent<roomPropLink>().unloadProps();}                               // Unload room props
-            catch(NullReferenceException e){ Debug.Log(e + "No proplink found, skipping..."); }                                           
+            catch(NullReferenceException e){ var x = e; Debug.Log("Skipping props"); }                                           
             currRoom.SetActive(false);                                                              // Unload room
 
             toRoom.SetActive(true);                                                                 // Load new room
             try{toRoom.GetComponent<roomPropLink>().loadProps();}                                   // Load new room props
-            catch(NullReferenceException e){ Debug.Log(e + "No proplink found, skipping..."); } 
+            catch(NullReferenceException e){ var x = e; Debug.Log("Skipping props"); } 
 
             player.transform.position =                                                             // Move player
                 new Vector2(toDoor.transform.localPosition.x, playerY);                         
